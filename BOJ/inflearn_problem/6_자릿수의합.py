6cd08050b592e5f2d42a22f01e4aac1eb38c0257
kjy5947 <kjy5947@gmail.com>
2022-01-31 18:46:02 +0900

    6_자릿수의 합

--- BOJ/inflearn_problem/6_자릿수의합.py
+++ BOJ/inflearn_problem/6_자릿수의합.py
@@ -20,8 +20,30 @@
 # print(max_index)
 
 # 강사 풀이
+# def digit_sum(x):
+#     sum = 0
+#     for i in str(x):
+#         sum += int(i)
+#     return sum
+
+#내 풀이
+n = int(input())
+
+arr = list(map(int, input().split()))
+
+sumarr=list()
 def digit_sum(x):
-    sum = 0
-    for i in str(x):
-        sum += int(i)
-    return sum
+    remain = 0
+    sumval = 0
+    if x == 0:
+        return 0
+    else:
+        while x > 0:
+            sumval += (x % 10)
+            x = x // 10
+        return sumval
+
+for a in arr:
+    sumarr.append(digit_sum(a))
+
+print(arr[sumarr.index(max(sumarr))])
